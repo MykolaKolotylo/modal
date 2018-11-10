0dce10f23300d8f75de0cceb574c0f7ff5407601
Mykola <mykolakolotilo@gmail.com>
2018-11-10 23:20:10 +0200

    add modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-banking-online';
+  isOpen = false;
+
+  onClick() {
+    this.isOpen = !this.isOpen;
+  }
 }
